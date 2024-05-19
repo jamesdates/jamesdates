b804dafa873038a5e91ee7c4f943a1b129b93c87
jamesdates <emilrosas@gmail.com>
2024-05-19 17:58:15 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,7 +1,11 @@
 function MarkMe() {
   var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
-  birth.bindPopup("<b>Birth</b> Hey guys, this is wehre I was born before I was taken ");
+  birth.bindPopup("<b>Birth</b> Hey guys, this is wehre I was born before I was taken <a href="#" onClick="openHome1()">home</a>");
 
   var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
   home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.>");
+
+  function openHome1() {
+    home1.openPopup;
+  }
 }
