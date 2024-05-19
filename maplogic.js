f1cbdf58d3d26a0f7ea7aa7b8fe230778d702819
jamesdates <emilrosas@gmail.com>
2024-05-19 16:49:18 +0200

    Add files via upload

--- maplogic.js
+++ maplogic.js
@@ -1,14 +1,16 @@
-function initialise() {	
+function initialise() {
 	// define the tile layer properties and set the appropriate attribution
 	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
 
 	// create the map object
-	myMap = new L.Map('mapid');	
+	myMap = new L.Map('mapid');
 	// start the map in South-East England
-	myMap.setView(new L.LatLng(58.466781651989535, 8.755421841858654),50);
+	myMap.setView(new L.LatLng(58.466781651989535, 8.755421841858654),9);
 	// apply the properties of the map
-	myMap.addLayer(osm);  
-}
+	myMap.addLayer(osm);
 
+	// add markers?
+	MarkMe();
+}
--- markers.js
+++ markers.js
@@ -1,2 +1,4 @@
-var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
-birth.bindPopup("<b>Birth</b><b>Hey guys, this is wehre I was born!")
+function MarkMe() {
+  var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
+  birth.bindPopup("<b>Birth</b><b>Hey guys, this is wehre I was born!");
+}
