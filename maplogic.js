a7c1484aa7aa2bbc3ccff79d8bba84011574cfd1
jamesdates <emilrosas@gmail.com>
2024-05-19 18:43:30 +0200

    Update maplogic.js

--- maplogic.js
+++ maplogic.js
@@ -21,11 +21,11 @@ function initialise() {
 function addMarkers() {
     // Define birth marker
     var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
-    birth.bindPopup("<b>Birth</b> Hey guys, this is where I was born before I was taken <a href='#' onClick='openHome1()'>home</a>");
+    birth.bindPopup("<b>Birth</b> Hey guys, this is where I was born before I was taken <a href='#' onclick='openHome1()'>home</a>");
 
     // Define home1 marker
     var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
-    home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.");
+    home1.bindPopup("<b>1st Home</b> This is where I first lived for approximately 1 year, I believe. After this we moved, but luckily not very far.");
 
     // Function to open home1 marker's popup when the link in birth marker's popup is clicked
     window.openHome1 = function() {
