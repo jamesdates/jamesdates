df1f18c563ba36ec9f8008d701ba061209172aab
jamesdates <emilrosas@gmail.com>
2024-05-19 16:23:26 +0200

    Update eg6-2-maplogic.js

--- eg6-2-maplogic.js
+++ eg6-2-maplogic.js
@@ -7,7 +7,7 @@ function initialise() {
 	// create the map object
 	myMap = new L.Map('mapid');	
 	// start the map in South-East England
-	myMap.setView(new L.LatLng(51.3, 0.7),9);
+	myMap.setView(new L.LatLng(58.466781651989535, 8.755421841858654),9);
 	// apply the properties of the map
 	myMap.addLayer(osm);  
 }
