11aa5210add7cc8f9e12ba0380d9a67d840902ac
jamesdates <emilrosas@gmail.com>
2024-05-19 18:24:21 +0200

    Update maplogic.js

--- maplogic.js
+++ maplogic.js
@@ -1,31 +1,35 @@
 function initialise() {
-	// define the tile layer properties and set the appropriate attribution
-	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 100, attribution: osmAttrib});
+    // Define the tile layer properties and set the appropriate attribution
+    var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 100, attribution: osmAttrib});
 
-	// create the map object
-	myMap = new L.Map('mapid');
-	// start the map in South-East England
-	myMap.setView(new L.LatLng(58.466781651989535, 8.755421841858654),9);
-	// apply the properties of the map
-	myMap.addLayer(osm);
-	// add markers?
-	markMe();
+    // Create the map object
+    myMap = new L.Map('mapid');
+    // Start the map in South-East England
+    myMap.setView(new L.LatLng(58.466781651989535, 8.755421841858654), 9);
+    // Apply the properties of the map
+    myMap.addLayer(osm);
+
+    // Call MarkMe function after the map has been initialized
+    MarkMe();
 }
 
 function MarkMe() {
-  var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
-  birth.bindPopup("<b>Birth</b> Hey guys, this is wehre I was born before I was taken <a href="#" onClick='openHome1()'>home</a>");
+    var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
+    birth.bindPopup("<b>Birth</b> Hey guys, this is where I was born before I was taken <a href='#' onClick='openHome1()'>home</a>");
 
-  var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
-  home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.>");
+    var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
+    home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.");
 
-  function openHome1() {
-    home1.openPopup();
-  }
+    function openHome1() {
+        home1.openPopup();
+    }
 
-  birth.on('click', function(e) {
-    myMap.setView(e.latlng, 100); // Zoom to level 15 when marker1 is clicked
-});
+    birth.on('click', function(e) {
+        myMap.setView(e.latlng, 100); // Zoom to level 100 when marker1 is clicked
+    });
 }
+
+// Call the initialise function to set up the map
+
