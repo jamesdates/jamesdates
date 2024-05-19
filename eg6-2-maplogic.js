ca7d19b66336507295140ff128dd378235455bbb
jamesdates <emilrosas@gmail.com>
2024-05-19 16:12:25 +0200

    Add files via upload

---
+++ eg6-2-maplogic.js
@@ -0,0 +1,14 @@
+function initialise() {	
+	// define the tile layer properties and set the appropriate attribution
+	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+
+	// create the map object
+	myMap = new L.Map('mapid');	
+	// start the map in South-East England
+	myMap.setView(new L.LatLng(51.3, 0.7),9);
+	// apply the properties of the map
+	myMap.addLayer(osm);  
+}
+
