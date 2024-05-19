0b67d6fdf023528fd5502361e21b89faa2e7fb98
jamesdates <emilrosas@gmail.com>
2024-05-19 18:09:40 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,15 +1,15 @@
 function MarkMe() {
-  var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
-  birth.bindPopup("<b>Birth</b> Hey guys, this is wehre I was born before I was taken <a href="#" onClick="openHome1()">home</a>");
+    var birth = L.marker([58.466781651989535, 8.755421841858654]).addTo(myMap);
+    birth.bindPopup("<b>Birth</b> Hey guys, this is where I was born before I was taken <a href='#' onClick='openHome1()'>home</a>");
 
-  var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
-  home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.>");
+    var home1 = L.marker([58.65287758119509, 9.143629797084088]).addTo(myMap);
+    home1.bindPopup("<b>1st Home</b> This is where I first lived, for approximately 1 year, I believe. After this we moved, but luckily not very far.");
 
-  function openHome1() {
-    home1.openPopup;
-  }
+    function openHome1() {
+        home1.openPopup();
+    }
 
-  birth.on('click', function(e) {
-    map.setView(e.latlng, 100); // Zoom to level 15 when marker1 is clicked
-});
+    birth.on('click', function(e) {
+        myMap.setView(e.latlng, 100); // Zoom to level 100 when marker1 is clicked
+    });
 }
